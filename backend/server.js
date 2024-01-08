aa479295d49138dab35d4d03e7c3015f77e78211
ClaytonV-EarthShot <clayton.vanimschoot@earthshot.ca>
2024-01-08 14:36:28 -0700

    Add some logs, handlers, and listeners

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
+require('dotenv'). config();
+
 const express = require('express');
+
+// express
+const app = express()
+
+// Log requests
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+})
+
+// route handler
+app.get('/', (req, res) => {
+  res.json({mssg: 'Successfully reached the front page front end'});
+})
+
+// listener
+app.listen(process.env.PORT, () => {
+  console.log('Successful listening on port', process.env.PORT);
+})
