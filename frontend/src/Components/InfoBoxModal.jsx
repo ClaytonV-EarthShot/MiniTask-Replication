24f6532ac91a9a0e5b97f3f3626c1da5fea03a78
ClaytonV-EarthShot <clayton.vanimschoot@earthshot.ca>
2024-02-01 17:59:26 -0700

    Issues fixed for scaling again. Fixed up information tab. Maps need to be added.

--- frontend/src/Components/InfoBoxModal.jsx
+++ frontend/src/Components/InfoBoxModal.jsx
@@ -1,22 +1,22 @@
 import { useState, useEffect } from "react";
 import DoughnutComp from "./DoughnutComp";
 import BarComp from "./BarComp";
+import ModalInfoTab from "./ModalInfoTab";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCloudRain, faTractor } from '@fortawesome/free-solid-svg-icons';
+import { faCloudRain, faTractor } from "@fortawesome/free-solid-svg-icons";
 
-export default function InfoBoxModal({country, visibility, closeModal}) {
-  
-  let imageName = '';
+export default function InfoBoxModal({ country, visibility, closeModal }) {
+  let imageName = "";
 
-  if(country.imageLink) {
+  if (country.imageLink) {
     imageName = `url('${country.imageLink})`;
   }
-  
-  const [tabOne, setTabOne] = useState('clicked');
-  const [tabTwo, setTabTwo] = useState('');
-  const [tabThree, setTabThree] = useState('');
-  const [tabFour, setTabFour] = useState('');
-  const [tabFive, setTabFive] = useState('');
+
+  const [tabOne, setTabOne] = useState("clicked");
+  const [tabTwo, setTabTwo] = useState("");
+  const [tabThree, setTabThree] = useState("");
+  const [tabFour, setTabFour] = useState("");
+  const [tabFive, setTabFive] = useState("");
   const [bucketOne, setBucketOne] = useState();
 
   useEffect(() => {
@@ -27,142 +27,185 @@ export default function InfoBoxModal({country, visibility, closeModal}) {
       if (response.ok) {
         setBucketOne(json);
       }
-    }
-
+    };
     fetchBucketOne();
   }, []);
 
-  console.log('This is the bucketOne object:');
-  console.dir(bucketOne);
-
   const handleChildElementClick = (e) => {
-    e.stopPropagation()
- }
+    e.stopPropagation();
+  };
 
   const tabClicked = (tab) => {
-    switch(tab) {
-      case 'one':
-        setTabOne('clicked');
-        setTabTwo('');
-        setTabThree('');
-        setTabFour('');
-        setTabFive('');
+    switch (tab) {
+      case "one":
+        setTabOne("clicked");
+        setTabTwo("");
+        setTabThree("");
+        setTabFour("");
+        setTabFive("");
         break;
-      case 'two':
-        setTabOne('');
-        setTabTwo('clicked');
-        setTabThree('');
-        setTabFour('');
-        setTabFive('');
+      case "two":
+        setTabOne("");
+        setTabTwo("clicked");
+        setTabThree("");
+        setTabFour("");
+        setTabFive("");
         break;
-      case 'three':
-        setTabOne('');
-        setTabTwo('');
-        setTabThree('clicked');
-        setTabFour('');
-        setTabFive('');
+      case "three":
+        setTabOne("");
+        setTabTwo("");
+        setTabThree("clicked");
+        setTabFour("");
+        setTabFive("");
         break;
-      case 'four':
-        setTabOne('');
-        setTabTwo('');
-        setTabThree('');
-        setTabFour('clicked');
-        setTabFive('');
+      case "four":
+        setTabOne("");
+        setTabTwo("");
+        setTabThree("");
+        setTabFour("clicked");
+        setTabFive("");
         break;
-      case 'five':
-        setTabOne('');
-        setTabTwo('');
-        setTabThree('');
-        setTabFour('');
-        setTabFive('clicked');
+      case "five":
+        setTabOne("");
+        setTabTwo("");
+        setTabThree("");
+        setTabFour("");
+        setTabFive("clicked");
         break;
     }
-  }
+  };
 
   return (
     <>
-    {visibility && (
-      <div className='infoBoxModalWrapper' onClick={closeModal}>
-          <div className='infoBoxModalContainer' onClick={(e) => handleChildElementClick(e)}>
-            <div className='modalHeader' style={{
-                backgroundImage: 
-                  `radial-gradient(circle, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.6) 70%, rgba(0,0,0,0.7) 100%), 
-                  ${imageName}`
-            }}>
-              <div className='modalTitleContainer'>
-                <div style={{display: 'flex', flexDirection: "column", marginTop: '0.5em', fontSize: '1.5em', textAlign: 'center', width: '63%', alignItems: 'flex-end'}}>
-                    <p style={{marginTop: '0.2em'}}>{country.country}<br/>{country.region}</p>
+      {visibility && (
+        <div className="infoBoxModalWrapper" onClick={closeModal}>
+          <div
+            className="infoBoxModalContainer"
+            onClick={(e) => handleChildElementClick(e)}
+          >
+            <div
+              className="modalHeader"
+              style={{
+                backgroundImage: `radial-gradient(circle, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.6) 70%, rgba(0,0,0,0.7) 100%), 
+                  ${imageName}`,
+              }}
+            >
+              <div className="modalTitleContainer">
+                <div className="modalTitleCountryRegion">
+                  <p>
+                    {country.country}
+                    <br />
+                    {country.region}
+                  </p>
                 </div>
-                <div style={{display: 'flex', justifyContent: 'flex-end',alignItems: 'flex-start', width: '33%'}}>
-                  <button className='exitButton' onClick={closeModal}>X</button>
+                <div className="modalTitleExitContainer">
+                  <button className="exitButton" onClick={closeModal}>
+                    X
+                  </button>
                 </div>
               </div>
-              <div className='modalButtonContainer'>
-                <button className={`modalButton ${tabOne}`} onClick={() => tabClicked('one')}>Information</button>
-                <button className={`modalButton ${tabTwo}`} onClick={() => tabClicked('two')}>Food & Agriculture</button>
-                <button className={`modalButton ${tabThree}`} onClick={() => tabClicked('three')}>Humanitarian</button>
-                <button className={`modalButton ${tabFour}`} onClick={() => tabClicked('four')}>Category</button>
-                <button className={`modalButton ${tabFive}`} onClick={() => tabClicked('five')}>Category</button>
+              <div className="modalButtonContainer">
+                <button
+                  className={`modalButton ${tabOne}`}
+                  onClick={() => tabClicked("one")}
+                >
+                  Information
+                </button>
+                <button
+                  className={`modalButton ${tabTwo}`}
+                  onClick={() => tabClicked("two")}
+                >
+                  Food & Agriculture
+                </button>
+                <button
+                  className={`modalButton ${tabThree}`}
+                  onClick={() => tabClicked("three")}
+                >
+                  Humanitarian
+                </button>
+                <button
+                  className={`modalButton ${tabFour}`}
+                  onClick={() => tabClicked("four")}
+                >
+                  Category
+                </button>
+                <button
+                  className={`modalButton ${tabFive}`}
+                  onClick={() => tabClicked("five")}
+                >
+                  Category
+                </button>
               </div>
-          </div>
-          <div className='modalContentWrapper'>
-              <div className='modalContentContainer'>
-                <div className='modalContent'>
-                  {tabOne === 'clicked' &&
+            </div>
+            <div className="modalContentWrapper">
+              <div className="modalContentContainer">
+                <div className="modalContent">
+                  {tabOne === "clicked" && (
                     <>
-                      <div className='infoTabContent'>
-                        <div>
-                          <p>{country.incomeClassification}</p>
-                        </div>
-                        <div>
-                        
-                        </div>
-                        <div>
-
-                        </div>
-                        <div>
-
-                        </div>
-                      </div>
-                      <div className='infoTabMapLinks'>
-                        
-                      </div>
+                      <ModalInfoTab country={country} />
                     </>
-                  }
-                  {tabTwo === 'clicked' &&
+                  )}
+                  {tabTwo === "clicked" && (
                     <>
-                      <div className='foodAndAgrWrapper'>
+                      <div className="foodAndAgrWrapper">
                         <h1>What Nutrients Is The Public Lacking?</h1>
-                        <div className='fAANutrition'>
+                        <div className="fAANutrition">
                           <span>
                             <DoughnutComp />
                           </span>
-                          <div className='fAANutritionFactList'>
-                            <div className='fAANutritionFactPositive'>
-                              <p className='fAANutritionFactItem'>🟢 Lowest deficieny is: </p>
-                              <p className='fAANutritionFactItem'>🟢 Lowest deficieny is: </p>
-                              <p className='fAANutritionFactItem'>🟢 Lowest deficieny is: </p>
+                          <div className="fAANutritionFactList">
+                            <div className="fAANutritionFactPositive">
+                              <p className="fAANutritionFactItem">
+                                🟢 Lowest deficieny is:{" "}
+                              </p>
+                              <p className="fAANutritionFactItem">
+                                🟢 Lowest deficieny is:{" "}
+                              </p>
+                              <p className="fAANutritionFactItem">
+                                🟢 Lowest deficieny is:{" "}
+                              </p>
                             </div>
-                            <div className='fAANutritionFactNegative'>
-                              <p className='fAANutritionFactItem'>🔴 Highest deficiency is: </p>
-                              <p className='fAANutritionFactItem'>🔴 Highest deficiency is: </p>
-                              <p className='fAANutritionFactItem'>🔴 Highest deficiency is: </p>
+                            <div className="fAANutritionFactNegative">
+                              <p className="fAANutritionFactItem">
+                                🔴 Highest deficiency is:{" "}
+                              </p>
+                              <p className="fAANutritionFactItem">
+                                🔴 Highest deficiency is:{" "}
+                              </p>
+                              <p className="fAANutritionFactItem">
+                                🔴 Highest deficiency is:{" "}
+                              </p>
                             </div>
                           </div>
                         </div>
                         <h1>Irrigated vs. Rainfed Crops</h1>
-                        <div className='fAACrops'>
-                          <div className='fAACropFacts'>
-                            <div className='fAACropFactItem'>
-                              <FontAwesomeIcon icon={faCloudRain} className='fAACropIcon' style={{color: 'rgb(101, 206, 241)'}}/>
-                              <p>In {country.country}, the crops are primarily rainfed</p>
+                        <div className="fAACrops">
+                          <div className="fAACropFacts">
+                            <div className="fAACropFactItem">
+                              <FontAwesomeIcon
+                                icon={faCloudRain}
+                                className="fAACropIcon"
+                                style={{ color: "rgb(101, 206, 241)" }}
+                              />
+                              <p>
+                                In {country.country}, the crops are primarily
+                                rainfed
+                              </p>
                             </div>
-                            <div className='fAACropFactItem'>
-                              <FontAwesomeIcon icon={faTractor} className='fAACropIcon' style={{color: 'rgb(241, 241, 101)'}}/>
+                            <div className="fAACropFactItem">
+                              <FontAwesomeIcon
+                                icon={faTractor}
+                                className="fAACropIcon"
+                                style={{ color: "rgb(241, 241, 101)" }}
+                              />
                               <p>Most Irrigated Crop: </p>
                             </div>
-                            <div className='fAACropFactItem'>
-                              <FontAwesomeIcon icon={faCloudRain} className='fAACropIcon' style={{color: 'rgb(101, 206, 241)'}}/>
+                            <div className="fAACropFactItem">
+                              <FontAwesomeIcon
+                                icon={faCloudRain}
+                                className="fAACropIcon"
+                                style={{ color: "rgb(101, 206, 241)" }}
+                              />
                               <p>Most Rainfed Crop: </p>
                             </div>
                           </div>
@@ -170,7 +213,7 @@ export default function InfoBoxModal({country, visibility, closeModal}) {
                             <BarComp />
                           </span>
                         </div>
-                        <div className='fAAAgriculutureFacts'>
+                        <div className="fAAAgriculutureFacts">
                           <table>
                             <tr>
                               <td>Cultivatable Land</td>
@@ -189,7 +232,10 @@ export default function InfoBoxModal({country, visibility, closeModal}) {
                               <td>0.97</td>
                             </tr>
                             <tr>
-                              <td>Public Spenditure on Agriculture (Percent of GDP)</td>
+                              <td>
+                                Public Spenditure on Agriculture (Percent of
+                                GDP)
+                              </td>
                               <td>527,714,000.00 ha</td>
                             </tr>
                             <tr>
@@ -200,14 +246,23 @@ export default function InfoBoxModal({country, visibility, closeModal}) {
                         </div>
                         <div className="fAABadOutcome">
                           <h1>Bad Outcome: Malnutrition</h1>
-                          <div className='fAABadOutcomeInfoWrapper'>
-                            <div className='fAABadOutcomeInfo'>
-                              <p>9% of the world are severly food insecure. This is 965 million people.</p>
-                              <p>25% of the world moderately or severly food insecure. This is 1.9 billion people</p>
+                          <div className="fAABadOutcomeInfoWrapper">
+                            <div className="fAABadOutcomeInfo">
+                              <p>
+                                9% of the world are severly food insecure. This
+                                is 965 million people.
+                              </p>
+                              <p>
+                                25% of the world moderately or severly food
+                                insecure. This is 1.9 billion people
+                              </p>
                               <p>8.9% of the world are undernourished</p>
-                              <p>22% of children in the world that are less than 5 years old are stunted.</p>
+                              <p>
+                                22% of children in the world that are less than
+                                5 years old are stunted.
+                              </p>
                             </div>
-                            <div className='fAABadOutcomeTableWrapper'>
+                            <div className="fAABadOutcomeTableWrapper">
                               <table>
                                 <tr>
                                   <td>Prevalence of Undernourishment</td>
@@ -239,46 +294,16 @@ export default function InfoBoxModal({country, visibility, closeModal}) {
                         </div>
                       </div>
                     </>
-                  }
-                  {tabThree === 'clicked' &&
-                    <div>
-                      You're in tab three.
-                    </div>
-                  }
-                  {tabFour === 'clicked' &&
-                    <div>
-                      You're in tab four.
-                    </div>
-                  }
-                  {tabFive === 'clicked' &&
-                    <div>
-                      You're in tab five.
-                    </div>
-                  }
+                  )}
+                  {tabThree === "clicked" && <div>You're in tab three.</div>}
+                  {tabFour === "clicked" && <div>You're in tab four.</div>}
+                  {tabFive === "clicked" && <div>You're in tab five.</div>}
                 </div>
               </div>
+            </div>
           </div>
         </div>
-      </div>
       )}
     </>
-  )
+  );
 }
-
-
-/* 
-
- style={{
-              backgroundImage: 
-                `radial-gradient(circle, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.6) 70%, rgba(0,0,0,0.7) 100%), 
-                ${imageName}`
-            }}
-
-const imageName = `url('${city.imageLinkName}')`;
-const mapName = `url('${city.mapLink}')`;
-
-<div style={{backgroundImage: mapName, height: '20em', width: '30em', backgroundSize: 'cover', backgroundPosition: 'center'}}>
-</div>
-
-*/
-
---
+++ frontend/src/Components/ModalInfoTab.jsx
@@ -0,0 +1,218 @@
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPeopleGroup,
+  faAngleUp,
+  faAngleDoubleUp,
+  faAngleDown,
+  faAngleDoubleDown,
+  faPerson,
+  faPlug,
+  faWifi,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function ModalInfoTab({ country }) {
+  const [progressWidth, setProgressWidth] = useState("");
+  const [incomeClass, setIncomeClass] = useState("");
+
+  let formattedPopulation = country.population.toLocaleString();
+
+  useEffect(() => {
+    const getProgressWidth = () => {
+      const progressWidthCalculation = Math.round(
+        (country.population / 1500000000) * 100
+      );
+      const progressWidth = progressWidthCalculation.toString() + "%";
+      setProgressWidth(progressWidth);
+    };
+
+    getProgressWidth();
+  }, [country.population]);
+
+  useEffect(() => {
+    const getIncomeClass = () => {
+      const subString = country.incomeClassification.substring(0, 3);
+      console.log(`This is substring: ${subString}`);
+      switch (subString) {
+        case "[UM":
+          setIncomeClass("upperMiddle");
+          break;
+        case "[HI":
+          setIncomeClass("highIncome");
+          break;
+        case "[LM":
+          setIncomeClass("lowMiddle");
+          break;
+        case "[LI":
+          setIncomeClass("lowIncome");
+          break;
+        default:
+          setIncomeClass("");
+          break;
+      }
+    };
+
+    getIncomeClass();
+  }, [country.incomeClass]);
+
+  return (
+    <>
+      <div className="infoTabContent">
+        <h1>Income Classification</h1>
+        <div className="incomeClassContainer">
+          {incomeClass === "upperMiddle" && (
+            <>
+              <FontAwesomeIcon
+                icon={faAngleUp}
+                style={{
+                  fontSize: "2em",
+                  marginRight: "0.5em",
+                  color: "rgb(76, 192, 38)",
+                }}
+              />
+              <p>
+                Upper Middle Income Economy. Gross National Income of $4256 -
+                $13205
+              </p>
+            </>
+          )}
+          {incomeClass === "highIncome" && (
+            <>
+              <FontAwesomeIcon
+                icon={faAngleDoubleUp}
+                style={{
+                  fontSize: "2em",
+                  marginRight: "0.5em",
+                  color: "rgb(76, 192, 38)",
+                }}
+              />
+              <p>
+                High Income Economy. Gross National Income of $13205 or more.
+              </p>
+            </>
+          )}
+          {incomeClass === "lowMiddle" && (
+            <>
+              <FontAwesomeIcon
+                icon={faAngleDown}
+                style={{
+                  fontSize: "2em",
+                  marginRight: "0.5em",
+                  color: "rgb(192, 38, 38)",
+                }}
+              />
+              <p>
+                Lower Middle Income Economy. Gross National Income of $1086 -
+                $4255
+              </p>
+            </>
+          )}
+          {incomeClass === "lowIncome" && (
+            <>
+              <FontAwesomeIcon
+                icon={faAngleDoubleDown}
+                style={{
+                  fontSize: "2em",
+                  marginRight: "0.5em",
+                  color: "rgb(192, 38, 38)",
+                }}
+              />
+              <p>Low Income Economy. Gross National Income of $1085 or less.</p>
+            </>
+          )}
+          {incomeClass === "" && (
+            <>
+              <p>No Recorded Data.</p>
+            </>
+          )}
+        </div>
+        <div className="gniTable">
+          <div className="gniContent">
+            <p>GNI Per Capita - Atlas Method</p>
+            <p>${country.gniAtlas.toLocaleString()}</p>
+          </div>
+          <div className="gniContent">
+            <p>GNI Per Capita - Purchasing Power Parity</p>
+            <p>${country.gniPPP.toLocaleString()}</p>
+          </div>
+        </div>
+        <h1>Population</h1>
+        <div className="populationBarContainer">
+          <FontAwesomeIcon
+            icon={faPeopleGroup}
+            style={{ marginRight: "1em", color: "rgb(192, 192, 38)" }}
+          />
+          <div className="populationBar">
+            <p>{formattedPopulation} people</p>
+            <div
+              className="populationBarProgress"
+              style={{ width: progressWidth }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="ruralLife">
+        <h1>Rural Life</h1>
+        <div className="ruralContentWrapper">
+          <div className="ruralLifeContentContainer">
+            <div className="ruralContent">
+              <FontAwesomeIcon
+                icon={faPerson}
+                style={{ fontSize: "1.5em", color: "rgb(38, 192, 192)" }}
+              />
+              <h4>Rural Population</h4>
+              <p>100000</p>
+            </div>
+            <div className="ruralContent">
+              <FontAwesomeIcon
+                icon={faPlug}
+                style={{ fontSize: "1.5em", color: "	rgb(192, 192, 38)" }}
+              />
+              <h4>Rural Electricity Access</h4>
+              <p>100000</p>
+            </div>
+            <div className="ruralContent">
+              <FontAwesomeIcon
+                icon={faWifi}
+                style={{ fontSize: "1.5em", color: "rgb(38, 192, 76)" }}
+              />
+              <h4>Rural Internet Connectivity</h4>
+              <p>100000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ruralLife">
+        <h1>Urban Life</h1>
+        <div className="ruralContentWrapper">
+          <div className="ruralLifeContentContainer">
+            <div className="ruralContent">
+              <FontAwesomeIcon
+                icon={faPerson}
+                style={{ fontSize: "1.5em", color: "rgb(38, 192, 192)" }}
+              />
+              <h4>Urban Population</h4>
+              <p>100000</p>
+            </div>
+            <div className="ruralContent">
+              <FontAwesomeIcon
+                icon={faPlug}
+                style={{ fontSize: "1.5em", color: "	rgb(192, 192, 38)" }}
+              />
+              <h4>Urban Electricity Access</h4>
+              <p>100000</p>
+            </div>
+            <div className="ruralContent">
+              <FontAwesomeIcon
+                icon={faWifi}
+                style={{ fontSize: "1.5em", color: "rgb(38, 192, 76)" }}
+              />
+              <h4>Urban Internet Connectivity</h4>
+              <p>100000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
